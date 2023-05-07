ff659cc960032793264e257414af71fc3ae764e5
Indrakant Dana <ik.dana24@gmail.com>
2023-05-07 15:56:28 +0530

    Add Components

---
+++ src/Components/Content.js
@@ -0,0 +1,160 @@
+import { useState } from "react";
+import { ChartBarIcon, ChartPieIcon } from "@heroicons/react/solid";
+import SideOvers from "./SideOvers";
+
+const musics = [
+  {
+    id: "1",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-1",
+    title: "Calm Reflections",
+    unique_plays: 1200,
+    total_plays: 2500,
+    completion_rate: 85,
+  },
+  {
+    id: "2",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-2",
+    title: "Tranquil Serenity",
+    unique_plays: 800,
+    total_plays: 1500,
+    completion_rate: 91,
+  },
+  {
+    id: "3",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-3",
+    title: "Meditative Melodies",
+    unique_plays: 2500,
+    total_plays: 3500,
+    completion_rate: 78,
+  },
+  {
+    id: "4",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-4",
+    title: "Soothing Symphony",
+    unique_plays: 1900,
+    total_plays: 2900,
+    completion_rate: 82,
+  },
+  {
+    id: "5",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-5",
+    title: "Peaceful Oasis",
+    unique_plays: 3000,
+    total_plays: 4500,
+    completion_rate: 75,
+  },
+  {
+    id: "6",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-6",
+    title: "Spiritual Harmony",
+    unique_plays: 1500,
+    total_plays: 2000,
+    completion_rate: 67,
+  },
+  {
+    id: "7",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-7",
+    title: "Zen Garden",
+    unique_plays: 2800,
+    total_plays: 3900,
+    completion_rate: 55,
+  },
+  {
+    id: "8",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-8",
+    title: "Relaxing Waters",
+    unique_plays: 2000,
+    total_plays: 3200,
+    completion_rate: 72,
+  },
+  {
+    id: "9",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-9",
+    title: "Mindful Moments",
+    unique_plays: 1400,
+    total_plays: 2100,
+    completion_rate: 88,
+  },
+  {
+    id: "10",
+    image: "https://source.unsplash.com/random/300x300?meditation-music-10",
+    title: "Serenity Now",
+    unique_plays: 1700,
+    total_plays: 2700,
+    completion_rate: 79,
+  },
+];
+
+const Content = () => {
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState(musics);
+  const [music, setMusic] = useState(null);
+  return (
+    <>
+      {<SideOvers open={open} setOpen={setOpen} music={music} />}
+      <div className="max-w-7xl mx-auto sm:px-6">
+        <h1 className="text-2xl font-semibold text-white">Dashboard</h1>
+      </div>
+      <div className="max-w-7xl mx-auto sm:px-6 my-8">
+        <div className="shadow overflow-hidden sm:rounded-md bg-gray-900 bg-opacity-30 backdrop-blur-lg">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 max-h-[80vh] overflow-y-auto scrollbar-thin scrollbar-thumb-blue-500/50 scrollbar-track-blue-600/50"
+          >
+            {data.map((music) => (
+              <li key={music.id}>
+                <div className="block hover:bg-gray-900 hover:bg-opacity-20 flex items-center px-4 py-4 sm:px-6">
+                  <div className="min-w-0 flex-1 flex items-center">
+                    <div className="flex-shrink-0">
+                      <img className="h-12 w-12 rounded-full" src={music.image} alt="" />
+                    </div>
+                    <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-3 md:gap-4">
+                      <p className="text-xl font-medium text-indigo-300 truncate flex items-center">{music.title}</p>
+                      <div className="hidden md:flex items-center justify-around">
+                        <div>
+                          <p className="text-sm text-gray-300">Total Plays</p>
+                          <p className="mt-2 flex items-center text-sm text-gray-200 font-semibold">
+                            <ChartBarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-sky-400" aria-hidden="true" />
+                            {music.total_plays}
+                          </p>
+                        </div>
+                        <div>
+                          <p className="text-sm text-gray-300">Unique Plays</p>
+                          <p className="mt-2 flex items-center text-sm text-gray-200 font-semibold">
+                            <ChartBarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-sky-400" aria-hidden="true" />
+                            {music.unique_plays}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="hidden md:block">
+                        <div className="flex justify-between mb-1">
+                          <span className="text-base font-medium text-blue-700 dark:text-white">Completition Rate</span>
+                          <span className="text-sm font-medium text-blue-700 dark:text-white">{music.completion_rate}%</span>
+                        </div>
+                        <div className="w-full bg-gray-400 bg-opacity-20 backdrop-blur-lg rounded-lg h-4">
+                          <div className="bg-blue-600 h-4 rounded-lg" style={{ width: music.completion_rate + "%" }}></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <ChartPieIcon
+                      className="h-5 w-5 text-indigo-500 cursor-pointer hover:text-indigo-300"
+                      aria-hidden="true"
+                      onClick={() => {
+                        setOpen(!open);
+                        setMusic(music);
+                      }}
+                    />
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Content;
---
+++ src/Components/SideOvers.js
@@ -0,0 +1,138 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+import React, { PureComponent } from "react";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+const data = [
+  {
+    date: "12/12/21",
+    unique_plays: 4000,
+    total_plays: 2400,
+  },
+  {
+    date: "12/13/21",
+    unique_plays: 3000,
+    total_plays: 1398,
+  },
+  {
+    date: "12/14/21",
+    unique_plays: 2000,
+    total_plays: 9800,
+  },
+  {
+    date: "12/15/21",
+    unique_plays: 2780,
+    total_plays: 3908,
+  },
+  {
+    date: "12/16/21",
+    unique_plays: 1890,
+    total_plays: 4800,
+  },
+  {
+    date: "12/17/21",
+    unique_plays: 2390,
+    total_plays: 3800,
+  },
+  {
+    date: "12/18/21",
+    unique_plays: 3490,
+    total_plays: 4300,
+  },
+];
+
+const SideOvers = ({ open, setOpen, music }) => {
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
+        <div className="absolute inset-0 overflow-hidden">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-100"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="absolute inset-0 bg-gray-800 bg-opacity-20 backdrop-blur-lg transition-opacity" />
+          </Transition.Child>
+
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <Transition.Child
+              as={Fragment}
+              enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
+            >
+              <div className="pointer-events-auto w-screen max-w-md">
+                <div className="flex h-full flex-col overflow-y-scroll bg-gray-900 bg-opacity-30 backdrop-blur-lg py-6 shadow-xl">
+                  <div className="px-4 sm:px-6">
+                    <div className="flex items-start justify-between">
+                      <Dialog.Title className="text-lg font-medium text-gray-300"> Statistics </Dialog.Title>
+                      <div className="ml-3 flex h-7 items-center">
+                        <button
+                          type="button"
+                          className="rounded-md text-gray-100 hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                          onClick={() => setOpen(false)}
+                        >
+                          <span className="sr-only">Close panel</span>
+                          <XIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                    <div className="absolute inset-0 px-4 sm:px-6">
+                      <div className="bg-white rounded-lg shadow w-full flex items-center justify-between p-6 space-x-6">
+                        <div className="flex-1 truncate">
+                          <p className="mt-1 text-gray-500 text-sm truncate">Music Name</p>
+                          <h3 className="text-gray-900 text-xl font-medium flex-1 truncate">{music?.title}</h3>
+                        </div>
+                        <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={music?.image} alt="" />
+                      </div>
+                      <div className="w-full mt-10">
+                        <ResponsiveContainer width="100%" aspect={1 / 1}>
+                          <BarChart width="100%" data={data} margin={{ top: 0, right: 30, left: -10, bottom: 30 }}>
+                            <XAxis dataKey="date" angle={-45} textAnchor="end" interval={0} tick={{ fontSize: 12 }} stroke="white" />
+                            <YAxis stroke="white" />
+                            <Tooltip cursor={{ fill: "transparent" }} />
+                            <Legend
+                              wrapperStyle={{
+                                position: "relative",
+                                marginTop: "10px",
+                                color: "white",
+                              }}
+                            />
+                            <defs>
+                              <linearGradient id="colorUv" x1="0" y1="0" x2="1" y2="0">
+                                <stop offset="10%" stopColor="#147eb7" />
+                                <stop offset="90%" stopColor="#5b4aff" />
+                              </linearGradient>
+                              <linearGradient id="colorPv" x1="0" y1="0" x2="1" y2="0">
+                                <stop offset="5%" stopColor="#04daff" />
+                                <stop offset="95%" stopColor="#090b72" />
+                              </linearGradient>
+                            </defs>
+                            <Bar dataKey="unique_plays" fill="url(#colorUv)" />
+                            <Bar dataKey="total_plays" fill="url(#colorPv)" />
+                          </BarChart>
+                        </ResponsiveContainer>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+};
+
+export default SideOvers;
